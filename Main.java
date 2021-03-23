e87dc65f7490b166e5463b866e8bdd475c5bb68b
darlynmendez <77795140+darlynmendez@users.noreply.github.com>
2021-03-23 14:31:46 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MyFrame welcome = new MyFrame();
+        LaunchSeasonalIngredientsPage launchPage01 = new LaunchSeasonalIngredientsPage();
+    }
+}
